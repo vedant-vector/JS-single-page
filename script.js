a45b5ab5198b7d708ff5f32d74927feb594fd576
vedant-simform <vedant@simformsolutions.com>
2023-02-08 19:17:05 +0530

    draft 1

---
+++ script.js
@@ -0,0 +1,66 @@
+const topHome = document.getElementById("topHome");
+const topAbout = document.getElementById("topAbout");
+const topImages = document.getElementById("topImages");
+const topServices = document.getElementById("topServices");
+
+function slideTo(name) {
+  var element = document.getElementById(name);
+  console.log(element);
+  element.scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+topHome.addEventListener("click", (e) => {
+  e.preventDefault();
+  slideTo("home");
+});
+
+topAbout.addEventListener("click", (e) => {
+  e.preventDefault();
+  slideTo("about");
+});
+topImages.addEventListener("click", (e) => {
+  e.preventDefault();
+  slideTo("images");
+});
+topServices.addEventListener("click", (e) => {
+  e.preventDefault();
+  slideTo("services");
+});
+
+const slides = document.querySelectorAll(".imageSlides");
+const pre = document.querySelector(".previous");
+const next = document.querySelector(".next");
+const imgF = document.getElementById("first-im");
+const imgS = document.getElementById("second-im");
+const imgT = document.getElementById("third-im");
+
+let index = 0;
+let cnt = 0;
+let negative = -1;
+let positive = 1;
+
+function show(num) {
+  let slides = document.querySelectorAll(".imgs");
+  if (num < 0) {
+    index = slides.length - 1;
+    cnt = 0;
+    slides[index].style.display = "block";
+  }
+  if (num >= 0) {
+    index = num;
+    slides[index].style.display = "block";
+  }
+}
+pre.addEventListener("click", () => {
+  cnt++;
+  index -= cnt;
+  show(index);
+});
+
+next.addEventListener("click", () => {
+  cnt++;
+  index += cnt;
+  show(index);
+});
